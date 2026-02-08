930e91ce105289f2fc7c7e824d89552014c850ff
Carlos Peralta <cperaltarayon@redventures.com>
2026-02-07 19:18:33 -0500

    nvim updates

--- nvim/.config/nvim/lua/carlos/plugins/auto-session.lua
+++ nvim/.config/nvim/lua/carlos/plugins/auto-session.lua
@@ -1,29 +1,29 @@
-return {
-  "rmagatti/auto-session",
-  lazy = false,
-
-  ---enables autocomplete for opts
-  ---@module "auto-session"
-  ---@type AutoSession.Config
-  opts = {
-    suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-    -- log_level = 'debug',
-  },
-}
-
 -- return {
 --   "rmagatti/auto-session",
---   config = function()
---     local auto_session = require("auto-session")
+--   lazy = false,
 --
---     auto_session.setup({
---       auto_restore_enabled = false,
---       auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
---     })
---
---     local keymap = vim.keymap
---
---     keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
---     keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
---   end,
+--   ---enables autocomplete for opts
+--   ---@module "auto-session"
+--   ---@type AutoSession.Config
+--   opts = {
+--     suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+--     -- log_level = 'debug',
+--   },
 -- }
+
+return {
+  "rmagatti/auto-session",
+  config = function()
+    local auto_session = require("auto-session")
+
+    auto_session.setup({
+      auto_restore_enabled = false,
+      auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
+    })
+
+    local keymap = vim.keymap
+
+    keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
+    keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
+  end,
+}
--- nvim/.config/nvim/lua/carlos/plugins/copilot.lua
+++ nvim/.config/nvim/lua/carlos/plugins/copilot.lua
@@ -20,7 +20,7 @@ return {
         },
       },
       suggestion = {
-        enabled = true,
+        enabled = false,
         auto_trigger = true,
         debounce = 75,
         keymap = {
