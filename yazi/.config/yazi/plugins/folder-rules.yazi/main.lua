968f287c54e87ac9987b2c37b39141e578212338
Carlos Peralta <cperaltarayon@redventures.com>
2026-02-19 02:33:02 -0500

    yazi updates

--- yazi/.config/yazi/plugins/folder-rules.yazi/main.lua
+++ yazi/.config/yazi/plugins/folder-rules.yazi/main.lua
@@ -1,7 +1,7 @@
 local function setup()
 	ps.sub("cd", function()
 		local cwd = cx.active.current.cwd
-		if cwd:ends_with("Downloads") then
+		if cwd:ends_with("Downloads") or cwd:ends_with("Desktop") then
 			ya.emit("sort", { "mtime", reverse = true, dir_first = false })
 		else
 			ya.emit("sort", { "alphabetical", reverse = false, dir_first = true })
