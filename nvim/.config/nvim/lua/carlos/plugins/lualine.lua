56a12b55642750f46145a36e18977ebd67f1095c
Carlos Peralta <cperaltarayon@redventures.com>
2026-02-23 01:45:34 -0500

    lualine custom theme

--- nvim/.config/nvim/lua/carlos/plugins/lualine.lua
+++ nvim/.config/nvim/lua/carlos/plugins/lualine.lua
@@ -4,11 +4,46 @@ return {
   config = function()
     local lualine = require("lualine")
     local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+    local C = require("catppuccin.palettes").get_palette("mocha")
 
-    -- configure lualine with catppuccin theme
+    local custom_catppuccin = {
+      normal = {
+        a = { bg = C.blue, fg = C.mantle, gui = "bold" },
+        b = { bg = C.surface0, fg = C.blue },
+        c = { bg = C.mantle, fg = C.text },
+      },
+      insert = {
+        a = { bg = C.green, fg = C.base, gui = "bold" },
+        b = { bg = C.surface0, fg = C.green },
+      },
+      terminal = {
+        a = { bg = C.green, fg = C.base, gui = "bold" },
+        b = { bg = C.surface0, fg = C.green },
+      },
+      command = {
+        a = { bg = C.peach, fg = C.base, gui = "bold" },
+        b = { bg = C.surface0, fg = C.peach },
+      },
+      visual = {
+        a = { bg = C.mauve, fg = C.base, gui = "bold" },
+        b = { bg = C.surface0, fg = C.mauve },
+      },
+      replace = {
+        a = { bg = C.red, fg = C.base, gui = "bold" },
+        b = { bg = C.surface0, fg = C.red },
+      },
+      inactive = {
+        a = { bg = C.mantle, fg = C.blue },
+        b = { bg = C.mantle, fg = C.surface1, gui = "bold" },
+        c = { bg = C.mantle, fg = C.overlay0 },
+      },
+    }
+
     lualine.setup({
       options = {
-        theme = "catppuccin",
+        theme = custom_catppuccin,
+        component_separators = { left = "│", right = "│" },
+        section_separators = { left = "", right = "" },
       },
       sections = {
         lualine_c = {
@@ -18,7 +53,7 @@ return {
           {
             lazy_status.updates,
             cond = lazy_status.has_updates,
-            color = { fg = "#ff9e64" },
+            color = { fg = C.peach },
           },
           { "encoding" },
           { "fileformat" },
