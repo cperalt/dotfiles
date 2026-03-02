60cad2ed6fdc1277312187b17a2b00c00904ecd2
Carlos Peralta <cperaltarayon@redventures.com>
2026-03-01 22:21:39 -0500

    add neotest

---
+++ nvim/.config/nvim/lua/carlos/plugins/neotest.lua
@@ -0,0 +1,48 @@
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-neotest/neotest-jest",
+  },
+  config = function()
+    local neotest = require("neotest")
+
+    neotest.setup({
+      adapters = {
+        require("neotest-jest")({
+          jestCommand = "npm test --",
+          jestConfigFile = "jest.config.js",
+          env = { CI = true },
+          cwd = function()
+            return vim.fn.getcwd()
+          end,
+        }),
+      },
+    })
+
+    local keymap = vim.keymap
+
+    keymap.set("n", "<leader>tr", function()
+      neotest.run.run()
+    end, { desc = "Run nearest test" })
+
+    keymap.set("n", "<leader>tf", function()
+      neotest.run.run(vim.fn.expand("%"))
+    end, { desc = "Run current file" })
+
+    keymap.set("n", "<leader>ts", function()
+      neotest.summary.toggle()
+    end, { desc = "Toggle test summary" })
+
+    keymap.set("n", "<leader>to", function()
+      neotest.output.open({ enter = true })
+    end, { desc = "Show test output" })
+
+    keymap.set("n", "<leader>tp", function()
+      neotest.output_panel.toggle()
+    end, { desc = "Toggle test output panel" })
+  end,
+}
