bf1ad89c6ce99857e37468ee0ff83c15a38309f3
Carlos Peralta <cperaltarayon@redventures.com>
2026-03-07 23:48:10 -0500

    which key add labels

--- nvim/.config/nvim/lua/carlos/plugins/which-key.lua
+++ nvim/.config/nvim/lua/carlos/plugins/which-key.lua
@@ -6,8 +6,34 @@ return {
     vim.o.timeoutlen = 500
   end,
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    plugins = {
+      marks = true,
+      registers = true,
+      presets = {
+        operators = false,
+        motions = false,
+        text_objects = false,
+        windows = false,
+        nav = false,
+        z = true,
+        g = false,
+      },
+    },
+    spec = {
+      { "<leader>c", group = "Code" },
+      { "<leader>d", group = "Diagnostics" },
+      { "<leader>e", group = "Explorer" },
+      { "<leader>f", group = "Find" },
+      { "<leader>g", group = "Git" },
+      { "<leader>h", group = "Hunk" },
+      { "<leader>l", group = "Lint" },
+      { "<leader>m", group = "Format" },
+      { "<leader>r", group = "Refactor" },
+      { "<leader>s", group = "Split" },
+      { "<leader>t", group = "Test" },
+      { "<leader>w", group = "Session" },
+      { "<leader>x", group = "Trouble" },
+      { "<leader><Tab>", group = "Tabs" },
+    },
   },
 }
