3b5e957388c18a6041be632d82ba2c95abfed898
Carlos Peralta <cperaltarayon@redventures.com>
2026-03-31 12:01:28 -0400

    rm jk to exit visual mode change

--- nvim/.config/nvim/lua/carlos/core/keymaps.lua
+++ nvim/.config/nvim/lua/carlos/core/keymaps.lua
@@ -6,9 +6,8 @@ local keymap = vim.keymap -- for conciseness
 ---------------------
 -- General Keymaps -------------------
 
--- use jk to exit insert/visual mode
+-- use jk to exit insert
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
-keymap.set("v", "jk", "<ESC>", { desc = "Exit visual mode with jk" })
 
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
