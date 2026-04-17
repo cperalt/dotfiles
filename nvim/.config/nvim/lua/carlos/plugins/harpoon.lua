ef82c399c9a368f56e175c5582c6467dbf1b5bdc
Carlos Peralta <cperaltarayon@redventures.com>
2026-04-17 01:39:54 -0400

    fix harpoon keybinds and hide from which key

--- nvim/.config/nvim/lua/carlos/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/carlos/plugins/harpoon.lua
@@ -45,21 +45,11 @@ return {
     end, { desc = "Harpoon: Add file" })
 
     -- Quick nav to harpooned files by index
-    keymap.set("n", "<leader>1", function()
-      harpoon:list():select(1)
-    end, { desc = "Harpoon: Go to file 1" })
-
-    keymap.set("n", "<leader>2", function()
-      harpoon:list():select(2)
-    end, { desc = "Harpoon: Go to file 2" })
-
-    keymap.set("n", "<leader>3", function()
-      harpoon:list():select(3)
-    end, { desc = "Harpoon: Go to file 3" })
-
-    keymap.set("n", "<leader>4", function()
-      harpoon:list():select(4)
-    end, { desc = "Harpoon: Go to file 4" })
+    for i = 1, 4 do
+      keymap.set("n", "<leader>" .. i, function()
+        harpoon:list():select(i)
+      end, { desc = "which_key_ignore" })
+    end
 
     -- Cycle through harpoon list
     keymap.set("n", "<leader>mp", function()
